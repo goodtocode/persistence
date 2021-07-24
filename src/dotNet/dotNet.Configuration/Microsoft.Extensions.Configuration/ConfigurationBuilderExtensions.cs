91860dc9468c9c9fbd43744098bcefd9bb9e1999
Robert Good <robert.good@goodtocode.com>
2021-07-24 11:40:50 -0700

    Organized folders by new mid-level namepsace part

---
+++ src/dotNet/dotNet.Configuration/Microsoft.Extensions.Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System;
+
+namespace GoodToCode.Shared.Configuration
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfiguration AddAzureAppConfigurationWithSentinel(this ConfigurationBuilder item, string appConfigurationConnection, string sentinelAppConfigKey, string environment)
+        {
+            item.AddAzureAppConfiguration(options =>
+                            options
+                                .Connect(appConfigurationConnection)
+                                .ConfigureRefresh(refresh =>
+                                {
+                                    refresh.Register(sentinelAppConfigKey, refreshAll: true)
+                                           .SetCacheExpiration(new TimeSpan(0, 60, 0));
+                                })
+                                .Select(KeyFilter.Any, LabelFilter.Null)
+                                .Select(KeyFilter.Any, environment)
+                        );
+            return item.Build();
+        }
+
+        public static IConfiguration AddAzureAppConfigurationWithSentinel(this ConfigurationBuilder item, string appConfigurationConnection, string sentinelAppConfigKey)
+        {
+            return AddAzureAppConfigurationWithSentinel(item, appConfigurationConnection, sentinelAppConfigKey, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production");
+        }
+    }
+}
---
+++ src/dotNet/dotNet.EntityFramework/DbContext/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoodToCode.Shared.EntityFramework
+{
+    public interface IDbContext
+    {
+        EntityEntry<TEntity> Entry<TEntity>([NotNullAttribute] TEntity entity) where TEntity : class;
+        Task<int> SaveChangesAsync(CancellationToken token = default);
+    }
+}
