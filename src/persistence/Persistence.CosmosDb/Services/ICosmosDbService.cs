96780b3acc57d3870e171049c0ca16e09db45163
Robert Good <robert.good@goodtocode.com>
2021-08-07 09:46:57 -0700

    Required constructor parameters

---
+++ src/persistence/Persistence.CosmosDb/Services/ICosmosDbService.cs
@@ -0,0 +1,8 @@
+﻿using GoodToCode.Shared.Persistence.Abstractions;
+
+namespace GoodToCode.Shared.Persistence.CosmosDb
+{
+    public interface ICosmosDbService<T> : IPersistenceService<T> where T : class, IEntity
+    {
+    }
+}
