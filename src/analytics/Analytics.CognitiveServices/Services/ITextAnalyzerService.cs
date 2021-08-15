6e70481b22d8e1bc76e00ecc7f2a394b44a44906
Robert Good <robert.good@goodtocode.com>
2021-08-15 10:16:10 -0700

    Added all methods to ITextAnalyzerService

--- src/analytics/Analytics.CognitiveServices/Services/ITextAnalyzerService.cs
+++ src/analytics/Analytics.CognitiveServices/Services/ITextAnalyzerService.cs
@@ -1,4 +1,5 @@
-﻿using GoodToCode.Shared.Analytics.Abstractions;
+﻿using Azure.AI.TextAnalytics;
+using GoodToCode.Shared.Analytics.Abstractions;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,10 +7,17 @@ namespace GoodToCode.Shared.Analytics.CognitiveServices
 {
     public interface ITextAnalyzerService
     {
-        Task<string> DetectLanguageAsync(string text);
-        Task<KeyPhraseResult> ExtractKeyPhrasesAsync(string text);
         Task<ISentimentResult> AnalyzeSentimentAsync(string text);
+        Task<ISentimentResult> AnalyzeSentimentAsync(string text, string languageIso);
         Task<IList<ISentimentResult>> AnalyzeSentimentBatchAsync(string text);
+        Task<string> DetectLanguageAsync(string text);
+        Task<IEnumerable<EntityResult>> ExtractEntitiesAsync(string text);
+        Task<LinkedResult> ExtractEntityLinksAsync(string text);
+        Task<IEnumerable<IAnalyticsResult>> ExtractHealthcareEntitiesAsync(string text);
+        Task<KeyPhraseResult> ExtractKeyPhrasesAsync(string text);
+        Task<IEnumerable<OpinionResult>> ExtractOpinionAsync(string text);
         string[] SplitParagraph(string paragraph);
+        List<ISentimentResult> ToSentimentResult(AnalyzeSentimentResultCollection results, string languageIso);
+        ISentimentResult ToSentimentResult(DocumentSentiment result);
     }
 }
