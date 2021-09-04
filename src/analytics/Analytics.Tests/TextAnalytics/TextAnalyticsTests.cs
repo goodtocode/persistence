8e86d77279b69d0934e80b90b086c25516444b88
Robert Good <robert.good@goodtocode.com>
2021-09-04 16:19:36 -0700

    sentimentsentence does only sentence, sentiment returns overall sentiment

--- src/analytics/Analytics.Tests/TextAnalytics/TextAnalyticsTests.cs
+++ src/analytics/Analytics.Tests/TextAnalytics/TextAnalyticsTests.cs
@@ -132,10 +132,26 @@ namespace GoodToCode.Shared.Analytics
         [TestMethod]
         public async Task TextAnalytics_Sentiment()
         {
-            SutText = "I had the best day of my life. I wish you were there with me.";
+            //SutText = "I had the best day of my life. I wish you were there with me.";
+            SutText = $"This year was a very difficult challenging year.  The virtual platform did not work as planned--It started and stopped every few seconds.  It got hung up, and would not play.  Even sitting right next to my modem did not help.  I was so disappointed.  I was almost going to request a refund.  I contacted the AACN office and Studio 27 at least 10-12 times!  Also I received my gift box Friday evening5/28.  Evals.  also did not work correctly. The benefits were that we could see the on line on - demand sessions when they were posted.Also the audience was from all over the world - which was great!";
             try
             {
                 var sutResult = await service.AnalyzeSentimentAsync(SutText);
+                Assert.IsTrue(sutResult != null);
+            }
+            catch (AggregateException ex)
+            {
+                log.LogError(ex.Message, ex);
+            }
+        }
+
+        [TestMethod]
+        public async Task TextAnalytics_SentimentBatch()
+        {
+            SutText = $"This year was a very difficult challenging year.  The virtual platform did not work as planned--It started and stopped every few seconds.  It got hung up, and would not play.  Even sitting right next to my modem did not help.  I was so disappointed.  I was almost going to request a refund.  I contacted the AACN office and Studio 27 at least 10-12 times!  Also I received my gift box Friday evening5/28.  Evals.  also did not work correctly. The benefits were that we could see the on line on - demand sessions when they were posted.Also the audience was from all over the world - which was great!";
+            try
+            {
+                var sutResult = await service.AnalyzeSentimentSentencesAsync(SutText);
                 Assert.IsTrue(sutResult != null);
             }
             catch (AggregateException ex)
