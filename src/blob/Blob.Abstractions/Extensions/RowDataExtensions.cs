79c3ee00bc2f1d1b703bda5114766009f5d2a505
Robert Good <robert.good@goodtocode.com>
2021-10-20 09:53:02 -0700

    IEnum<IRowData> ToDictionary fixed

---
+++ src/blob/Blob.Abstractions/Extensions/RowDataExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace GoodToCode.Shared.Blob.Abstractions
+{
+    public static class RowDataExtensions
+    {
+        public static Dictionary<string, object> ToDictionary(this IEnumerable<IRowData> item)
+        {
+            var returnDict = new Dictionary<string, object>();
+            foreach (var row in item)
+                foreach (var cell in row.Cells)
+                    if (!string.IsNullOrWhiteSpace(cell.ColumnName) && cell.CellValue != null)
+                        returnDict.Add(cell.ColumnName, cell.CellValue);
+
+            return returnDict;
+        }
+
+        public static Dictionary<string, object> ToDictionary(this IEnumerable<RowData> item)
+        {
+            var returnDict = new Dictionary<string, object>();
+            foreach(var row in item)
+                foreach (var cell in row.Cells)
+                    if (!string.IsNullOrWhiteSpace(cell.ColumnName) && cell.CellValue != null)
+                        returnDict.Add(cell.ColumnName, cell.CellValue);
+
+            return returnDict;
+        }
+    }
+}
