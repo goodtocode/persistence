71eb642bff41c0c626527e7341f0a27059efa0c0
Robert Good <robert.good@goodtocode.com>
2022-02-20 19:20:10 -0800

    Added missing config values

--- src/spatial/Spatial.Tests/AppConfigurationKeys.cs
+++ src/spatial/Spatial.Tests/AppConfigurationKeys.cs
@@ -3,10 +3,5 @@
     public struct AppConfigurationKeys
     {
         public const string SentinelKey= "Sentinel";
-        public const string CognitiveServicesEndpoint= "Analytics:CognitiveService:Endpoint";
-        public const string CognitiveServicesKeyCredential= "Analytics:CognitiveService:KeyCredential";
-        public const string TextAnalyticsEndpoint= "Analytics:TextAnalytics:Endpoint";
-        public const string TextAnalyticsKeyCredential= "Analytics:TextAnalytics:KeyCredential";
-        public const string StorageTablesConnectionString= "Persistence:StorageTables:ConnectionString";
     }
 }
